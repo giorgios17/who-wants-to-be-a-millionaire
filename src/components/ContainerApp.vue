f9cf5f76834cc5f654c1ec04c2999e107e86bd37
giorgios17 <giosur@live.it>
2022-07-14 11:24:38 +0200

    init +  first container component

---
+++ src/components/ContainerApp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div id="logo-contaienr">
+      <img alt="logo" src="../assets/logo.png" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContainerApp",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
